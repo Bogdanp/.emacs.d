f91b42f78cacc411bec615352e4eabe16b8acf6f
Bogdan Popa <popa.bogdanp@gmail.com>
2013-09-12 20:51:07 +0300

    initial commit

---
+++ init.el
@@ -0,0 +1,116 @@
+;;; Package archives
+;;; ~~~~~~~~~~~~~~~~
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;;; Set-up and load packages.
+(setq my-packages
+     '(coffee-mode
+       dired+
+       evil
+       flymake-easy
+       goto-last-change
+       ido-ubiquitous
+       jedi
+       magit
+       markdown-mode
+       monky
+       paredit
+       rainbow-delimiters
+       rainbow-mode
+       scala-mode2
+       smex
+       twilight-theme
+       undo-tree))
+
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+       (package-install package)))
+  my-packages)
+
+(mapc
+ (lambda (package)
+   (require package))
+  my-packages)
+
+;;; Load ENSIME package.
+(add-to-list 'load-path "~/.emacs.d/packages/ensime/elisp/")
+
+;;; Jedi
+;;; ~~~~
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;;; VIM emulation
+;;; ~~~~~~~~~~~~~
+(evil-mode 1)
+
+;;; Add C-w shortcut.
+(global-set-key (kbd "C-w") 'backward-kill-word)
+
+;;; Scala mode
+;;; ~~~~~~~~~~
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+;;; Flymake
+;;; ~~~~~~~
+;;; Load pycheckers.
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pycheckers" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+;;; Fonts
+;;; ~~~~~
+(set-default-font "Inconsolata-15")
+
+;;; Themes
+;;; ~~~~~~
+(load-theme 'twilight t)
+
+;;; Auto completion
+;;; ~~~~~~~~~~~~~~~
+(global-auto-complete-mode t)
+(setq ac-auto-start nil)
+
+(ac-set-trigger-key "TAB")
+
+(define-key ac-complete-mode-map "\C-n" 'ac-next)
+(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
+;;; Line editing
+;;; ~~~~~~~~~~~~
+(setq evil-default-cursor t)
+
+;;; Highlight current line.
+(global-hl-line-mode t)
+
+;;; Don't wrap long lines.
+(setq-default truncate-lines t)
+
+;;; Make sure the cursor is white.
+(set-cursor-color "#ffffff")
+
+;;; Parentheses
+;;; ~~~~~~~~~~~
+(show-paren-mode 1)
+
+;;; Backups
+;;; ~~~~~~~
+(setq make-backup-files nil)
+
+;;; Misc
+;;; ~~~~
+(setq exec-path (append exec-path (list "/usr/local/bin")))
