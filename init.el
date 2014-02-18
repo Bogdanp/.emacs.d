7eb750b0a0a8125a588a9e1577e3b4555fd2bd17
Bogdan Popa <popa.bogdanp@gmail.com>
2014-02-18 20:01:03 +0200

    improve Haskell support

--- init.el
+++ init.el
@@ -28,7 +28,8 @@
     yaml-mode fsharp-mode
 
     ;; Haskell
-    haskell-mode flymake-haskell-multi ghc
+    ;; Handled below in el-get.
+    ;; haskell-mode flymake-haskell-multi ghc
 
     ;; Lisp
     rainbow-delimiters rainbow-mode starter-kit starter-kit-lisp
@@ -56,6 +57,30 @@
   my-packages)
 
 (mapc 'require my-required-packages)
+
+;; el-get
+;; ~~~~~~
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+
+;; Install el-get if it's not present.
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; Synchronize recipes.
+(defun el-get-sync-recipes (overlay)
+  (let* ((recipe-glob (locate-user-emacs-file (concat overlay "/recipes/*.rcp")))
+         (recipe-files (file-expand-wildcards recipe-glob))
+         (recipes (mapcar 'el-get-read-recipe-file recipe-files)))
+    (mapcar (lambda (r) (add-to-list 'el-get-sources r)) recipes)
+    (el-get 'sync (mapcar 'el-get-source-name recipes))))
+(setq el-get-user-package-directory user-emacs-directory)
+
+(el-get-sync-recipes "el-get-haskell")
+(el-get-sync-recipes "el-get-user")
 
 ;; Load ENSIME package.
 (add-to-list 'load-path "~/.emacs.d/packages/ensime/elisp/")
@@ -240,6 +265,7 @@ vice-versa).
 ;; Misc
 ;; ~~~~
 (setq exec-path (append exec-path (list "/usr/local/bin"
+                                        "/Users/bogdan/.cabal/bin"
                                         "/Applications/Racket v6.0.0.1/bin/")))
 
 ;; Bindings
