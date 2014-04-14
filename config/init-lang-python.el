b6b4d00a0823ae0b988ef974a85139dac355379c
Bogdan Popa <popa.bogdanp@gmail.com>
2014-04-14 23:21:04 +0300

    set up jedi mode for python properly

--- config/init-lang-python.el
+++ config/init-lang-python.el
@@ -1,5 +1,8 @@
 (add-hook 'python-mode-hook
           (lambda ()
+            (jedi:setup)
+            (setq-local jedi:complete-on-dot t)
+
             ;; Don't start automatically (causes SERIOUS performance issues on
             ;; large Python files (> 1k LOC)).
             (setq-local ac-auto-start nil)))
