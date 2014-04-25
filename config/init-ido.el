525bcec6ba0876c2c8503c48735ddaa52a29cd29
Bogdan Popa <popa.bogdanp@gmail.com>
2014-04-26 01:47:51 +0300

    make ido even more useful

--- config/init-ido.el
+++ config/init-ido.el
@@ -1,10 +1,13 @@
 (ido-mode t)
-(ido-ubiquitous-mode t)
+(ido-ubiquitous-mode)
 
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
       ido-create-new-buffer 'always
       ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
       ido-max-prospects 10)
 
 
