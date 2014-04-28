c9b70f0b78d9525fb3c078c285ecc9e61821b799
Bogdan Popa <popa.bogdanp@gmail.com>
2014-04-28 11:44:46 +0300

    add reminders to org agenda

--- config/init-org.el
+++ config/init-org.el
@@ -4,4 +4,31 @@
 ;; Set up path to agenda files.
 (setq org-agenda-files `(,(expand-file-name "~/Documents/Org/")))
 
+
+;; Reminders
+;; ~~~~~~~~~
+;; Code below mostly stolen from http://doc.norang.ca/org-mode.html#Reminders
+(defun org/agenda-to-appt ()
+  "Erase all current reminders and rebuild the list from the current
+agenda."
+  (interactive)
+  (setq appt-time-msg-list nil)
+  (org-agenda-to-appt))
+
+;; Display appointment info in the modeline.
+(setq appt-display-mode-line t)
+
+;; Rebuild reminders each time the agenda is displayed.
+(add-hook 'org-finalize-agenda-hook #'org/agenda-to-appt 'append)
+
+;; Activate appointments.
+(appt-activate t)
+
+;; Reset appointments 1 minute after midnight.
+(run-at-time "24:01" nil #'org/agenda-to-appt)
+
+;; Setup appointments at startup.
+(org/agenda-to-appt)
+
+
 (provide 'init-org)
