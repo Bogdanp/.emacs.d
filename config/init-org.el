d7a54fbbf03ad63d6d2df9ed848009b78c64d25b
Bogdan Popa <popa.bogdanp@gmail.com>
2014-04-28 11:20:07 +0300

    setup agenda files

--- config/init-org.el
+++ config/init-org.el
@@ -1,5 +1,7 @@
 ;; Highlight code in BEGIN_SRC-END_SRC blocks.
 (setq org-src-fontify-natively t)
 
+;; Set up path to agenda files.
+(setq org-agenda-files `(,(expand-file-name "~/Documents/Org/")))
 
 (provide 'init-org)
