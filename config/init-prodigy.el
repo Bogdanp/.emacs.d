5139ea26a6db202c1c09f9ef021a94cdb15ec176
Bogdan Popa <popa.bogdanp@gmail.com>
2014-05-01 11:23:18 +0300

    add defin.io to prodigy

--- config/init-prodigy.el
+++ config/init-prodigy.el
@@ -2,16 +2,25 @@
 
 (prodigy-define-service
   :name "LeadPages Server"
-  :command "/Users/bogdan/Work/lead-pages/runserver"
-  :cwd "/Users/bogdan/Work/lead-pages/"
+  :command (expand-file-name "~/Work/lead-pages/runserver")
+  :cwd (expand-file-name "~/Work/lead-pages/")
   :tags '(work)
   :stop-signal 'sigterm
   :kill-process-buffer-on-stop t)
 
 (prodigy-define-service
   :name "LeadPages AWeber Server"
-  :command "/Users/bogdan/Work/leadpages-integrations/AWeber/runserver"
-  :cwd "/Users/bogdan/Work/leadpages-integrations/AWeber/"
+  :command (expand-file-name "~/Work/leadpages-integrations/AWeber/runserver")
+  :cwd (expand-file-name "~/Work/leadpages-integrations/AWeber/")
+  :tags '(work)
+  :stop-signal 'sigterm
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "defn.io"
+  :command (expand-file-name "~/sandbox/defn/dist/build/defn/defn")
+  :cwd (expand-file-name "~/sandbox/defn/")
+  :args `("-p" "8093" ,(expand-file-name "~/sandbox/defn/articles/"))
   :tags '(work)
   :stop-signal 'sigterm
   :kill-process-buffer-on-stop t)
