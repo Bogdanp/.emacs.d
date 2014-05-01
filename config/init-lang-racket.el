9a1dfd403d0e26cc0192053a12048dc22c97868f
Bogdan Popa <popa.bogdanp@gmail.com>
2014-05-01 15:13:43 +0300

    fix geiser ac

--- config/init-lang-racket.el
+++ config/init-lang-racket.el
@@ -1,5 +1,11 @@
 (add-hook 'scheme-mode-hook 'paredit-mode)
 (add-hook 'scheme-mode-hook 'pretty-lambda-mode)
 
+(add-hook 'geiser-mode-hook 'ac-geiser-setup)
+(add-hook 'geiser-repl-mode-hook 'ac-geiser-setup)
+
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'geiser-repl-mode))
+
 
 (provide 'init-lang-racket)
