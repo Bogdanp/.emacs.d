c0f5aff5c85ac40c577bb536d443e323b32d8b20
Bogdan Popa <popa.bogdanp@gmail.com>
2014-05-08 14:16:02 +0300

    add more stuff to prodigy

--- config/init-prodigy.el
+++ config/init-prodigy.el
@@ -1,5 +1,9 @@
 (add-hook 'prodigy-mode-hook 'evil-emacs-state)
 
+(setq prodigy:screenshot-service-env
+      `(("PHANTOMJS_BIN_PATH" "/usr/local/bin/phantomjs")
+        ("PHANTOMJS_CAPTURE_PATH" ,(expand-file-name "~/Work/screenshot-service/phantomjs/capture.js"))))
+
 (prodigy-define-service
   :name "LeadPages Server"
   :command (expand-file-name "~/Work/lead-pages/runserver")
@@ -17,6 +21,26 @@
   :kill-process-buffer-on-stop t)
 
 (prodigy-define-service
+  :name "LeadPages Screenshot Service"
+  :command (expand-file-name "~/Work/screenshot-service/venv/bin/python")
+  :cwd (expand-file-name "~/Work/screenshot-service/")
+  :args `(,(expand-file-name "~/Work/screenshot-service/service/app.py") "5000")
+  :env  prodigy:screenshot-service-env
+  :tags '(work)
+  :stop-signal 'sigterm
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "LeadPages Screenshot Service Consumer"
+  :command (expand-file-name "~/Work/screenshot-service/venv/bin/python")
+  :cwd (expand-file-name "~/Work/screenshot-service/consumer/")
+  :args `(,(expand-file-name "~/Work/screenshot-service/consumer/consumer.py"))
+  :env prodigy:screenshot-service-env
+  :tags '(work)
+  :stop-signal 'sigterm
+  :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
   :name "defn.io"
   :command (expand-file-name "~/sandbox/defn/dist/build/defn/defn")
   :cwd (expand-file-name "~/sandbox/defn/")
@@ -24,5 +48,13 @@
   :tags '(personal)
   :stop-signal 'sigterm
   :kill-process-buffer-on-stop t)
+
+(prodigy-define-service
+  :name "beanstalkd"
+  :command "beanstalkd"
+  :tags '(personal work)
+  :stop-signal 'sigterm
+  :kill-process-buffer-on-stop t)
+
 
 (provide 'init-prodigy)
