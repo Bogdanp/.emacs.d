639b3a4242f9f13c431b4237d135cae00dd9e811
Bogdan Popa <popa.bogdanp@gmail.com>
2014-07-05 23:11:11 +0300

    ..

--- config/init-lang-ocaml.el
+++ config/init-lang-ocaml.el
@@ -1,10 +1,12 @@
 ;; Auto completion and error checking
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 (add-hook 'tuareg-mode-hook 'merlin-mode)
-(add-hook 'caml-mode-hook 'merlin-mode)
 
 ;; Make sure we use ac
 (setq merlin-use-auto-complete-mode t)
+
+(add-to-list 'auto-mode-alist '("\\.eliom" . tuareg-mode))
+
 
 
 (provide 'init-lang-ocaml)
