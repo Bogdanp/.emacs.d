52b4d5d9b68ce1d1473e8b5c1141a020add75d45
Bogdan Popa <popa.bogdanp@gmail.com>
2014-07-23 13:07:53 +0300

    make js2-mode less strict

--- config/init-lang-javascript.el
+++ config/init-lang-javascript.el
@@ -1,5 +1,10 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+(custom-set-variables
+ '(js2-mode-show-parse-errors nil)
+ '(js2-highlight-external-variables nil)
+ '(js2-mode-show-strict-warnings nil))
+
 (add-hook 'js2-mode-hook (lambda ()
                            (setq-local indent-tabs-mode nil)))
 
