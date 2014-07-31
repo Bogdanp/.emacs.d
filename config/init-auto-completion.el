75d3e98250eaf35831b561e844a5787c2b8bdc48
Bogdan Popa <popa.bogdanp@gmail.com>
2014-07-31 11:47:12 +0300

    improve ac

--- config/init-auto-completion.el
+++ config/init-auto-completion.el
@@ -4,6 +4,22 @@
 (ac-config-default)
 (ac-set-trigger-key "TAB")
 
+;; I don't mind having to press TAB.
 (setq ac-auto-start nil)
+
+;; Source ALL the things.
+(setq-default ac-sources '(ac-source-filename
+                           ac-source-functions
+                           ac-source-yasnippet
+                           ac-source-variables
+                           ac-source-symbols
+                           ac-source-features
+                           ac-source-abbrev
+                           ac-source-words-in-same-mode-buffers
+                           ac-source-dictionary))
+
+;; Smarcase completion.
+(setq ac-ignore-case 'smart)
+
 
 (provide 'init-auto-completion)
