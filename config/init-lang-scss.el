ecfe094664c8a09487b499be4a5196130faea0ab
Bogdan Popa <popa.bogdanp@gmail.com>
2014-08-02 18:03:11 +0300

    use 2 spaces to indent scss

--- config/init-lang-scss.el
+++ config/init-lang-scss.el
@@ -1,5 +1,10 @@
 ;; Stupid functionality is stupid.
 (setq scss-compile-at-save nil)
 
+;; Indentation
+;; ~~~~~~~~~~~
+(add-hook 'scss-mode-hook (lambda ()
+                            (setq-local css-indent-offset 2)))
+
 
 (provide 'init-lang-scss)
