7daa5868af74b9f52d6f7026b8825243d000fa54
Bogdan Popa <popa.bogdanp@gmail.com>
2014-11-15 12:59:56 +0200

    update frame code

--- config/init-frame.el
+++ config/init-frame.el
@@ -1,12 +1,12 @@
 (defun move-current-frame-to-1st-monitor ()
   (interactive)
-  (set-frame-size (selected-frame) 235 65)
+  (set-frame-size (selected-frame) 205 53)
   (set-frame-position (selected-frame) 10 32))
 
 (defun move-current-frame-to-2nd-monitor ()
   (interactive)
   (set-frame-size (selected-frame) 235 63)
-  (set-frame-position (selected-frame) 1930 32))
+  (set-frame-position (selected-frame) 1690 32))
 
 (defun create-frame-on-1st-monitor ()
   (interactive)
