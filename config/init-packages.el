fd7d8a4f7a81619fe5da603d52c0acc89d0a8d29
Bogdan Popa <popa.bogdanp@gmail.com>
2014-12-23 15:22:38 +0200

    add evil-surround as local package

--- config/init-packages.el
+++ config/init-packages.el
@@ -120,6 +120,7 @@ list of packages to be instaleld using package.el."
 
 ;; Packages that may or may not be available on disk.
 (bp-add-local-package 'evil)
+(bp-add-local-package 'evil-surround)
 
 (when (display-graphic-p)
   (bp-add-local-package 'twilight-anti-bright-theme))
