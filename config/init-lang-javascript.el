5124ac4e895fee13bde3740bd5708e67e5c476ee
Bogdan Popa <popa.bogdanp@gmail.com>
2015-01-03 16:32:16 +0200

    fix jsxhint checker

--- config/init-lang-javascript.el
+++ config/init-lang-javascript.el
@@ -8,7 +8,7 @@
 (flycheck-define-checker jsxhint-checker
   "A JSX syntax and style checker based on JSXHint."
 
-  :command ("jsxhint" source)
+  :command ("jsxhint" source-inplace)
   :error-patterns
   ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
   :modes (web-mode))
