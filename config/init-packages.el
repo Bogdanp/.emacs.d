5339661d274d3bd9a08c37ccf9c0ff4f29200b1f
Bogdan Popa <popa.bogdanp@gmail.com>
2015-01-10 20:13:34 +0200

    ..

--- config/init-packages.el
+++ config/init-packages.el
@@ -1,6 +1,7 @@
 ;;; * Setup
-(with-no-warnings (require 'cl)
-                  (require 'package))
+(with-no-warnings
+  (require 'cl)
+  (require 'package))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
