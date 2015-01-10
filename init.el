25016064249d2efc886fe4dc9baaa65cac1b7b36
Bogdan Popa <popa.bogdanp@gmail.com>
2015-01-10 11:16:29 +0200

    add use-package

--- init.el
+++ init.el
@@ -2,12 +2,19 @@
 ;; Auto compile everything
 ;; ~~~~~~~~~~~~~~~~~~~~~~~
 (setq load-prefer-newer t)
+
 (add-to-list 'load-path (locate-user-emacs-file "packed"))
 (add-to-list 'load-path (locate-user-emacs-file "auto-compile"))
 
 (require 'auto-compile)
 (auto-compile-on-load-mode 1)
 (auto-compile-on-save-mode 1)
+
+
+;; use-package
+;; ~~~~~~~~~~~
+(add-to-list 'load-path (locate-user-emacs-file "use-package"))
+(require 'use-package)
 
 
 ;; UI
