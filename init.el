ed6fd20a28eb0a577c35ad37f845c4872aadb9cc
Bogdan Popa <popa.bogdanp@gmail.com>
2015-11-27 16:56:11 +0200

    use smartparens for c

--- init.el
+++ init.el
@@ -1040,6 +1040,7 @@
   :commands smartparens-mode
   :init
   (add-hook 'elixir-mode-hook #'turn-on-smartparens-strict-mode)
+  (add-hook 'irony-mode-hook #'turn-on-smartparens-strict-mode)
   (add-hook 'python-mode-hook #'turn-on-smartparens-strict-mode)
   (add-hook 'org-mode-hook #'turn-on-smartparens-strict-mode)
   :config
