bf614c5e79a8b0dcf27da28964ba6f79e935f0c9
Bogdan Popa <popa.bogdanp@gmail.com>
2016-01-25 08:49:08 +0200

    switch to fira mono

--- init.el
+++ init.el
@@ -58,7 +58,7 @@
 ;;; UI
 ;; Position and resize frame.
 (when (window-system)
-  (add-to-list 'default-frame-alist '(font . "Inconsolata-13"))
+  (add-to-list 'default-frame-alist '(font . "fira mono-12"))
   (add-to-list 'default-frame-alist '(top . 32))
   (add-to-list 'default-frame-alist '(left . 10))
   (add-to-list 'default-frame-alist '(width . 199))
