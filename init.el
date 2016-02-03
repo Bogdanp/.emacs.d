b04c53cfc6e19dc2ffaf5a390344200d08cb1bd5
Bogdan Popa <popa.bogdanp@gmail.com>
2016-02-03 09:01:17 +0200

    increase font size

--- init.el
+++ init.el
@@ -58,7 +58,7 @@
 ;;; UI
 ;; Position and resize frame.
 (when (window-system)
-  (add-to-list 'default-frame-alist '(font . "ubuntu mono-12"))
+  (add-to-list 'default-frame-alist '(font . "ubuntu mono-14"))
   (add-to-list 'default-frame-alist '(top . 32))
   (add-to-list 'default-frame-alist '(left . 10))
   (add-to-list 'default-frame-alist '(width . 199))
