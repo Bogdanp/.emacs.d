cc2219c1dc34d0d45e54c1fa8d29aeb4a3462dbe
Bogdan Popa <popa.bogdanp@gmail.com>
2016-04-20 08:43:01 +0300

    disable atom-one-dark

--- init.el
+++ init.el
@@ -165,11 +165,11 @@
     :config (load-theme 'twilight-anti-bright t))
 
   (use-package better-default-theme
-    :disabled t
     :load-path "vendor/better-default-theme"
     :config (load-theme 'better-default t))
 
   (use-package atom-one-dark-theme
+    :disabled t
     :load-path "vendor/atom-one-dark-theme"
     :config (load-theme 'atom-one-dark t)))
 
