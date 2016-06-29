20df3d0fca7e2ebf357421256f79c883aa7a60ca
Bogdan Popa <popa.bogdanp@gmail.com>
2016-06-29 11:24:18 +0300

    set flycheck-flake8rc to "setup.cfg"

--- init.el
+++ init.el
@@ -868,7 +868,8 @@
   :init
   (add-hook 'prog-mode-hook #'flycheck-mode)
   (setq-default flycheck-emacs-lisp-load-path 'inherit)
-  (setq-default flycheck-disabled-checkers '(sass)))
+  (setq-default flycheck-disabled-checkers '(sass))
+  (setq-default flycheck-flake8rc "setup.cfg"))
 
 
 ;;; File navigation
