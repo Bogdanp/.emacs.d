97846d4e9a74d2cab7c3b0b8e6278583d8e3153f
Bogdan Popa <popa.bogdanp@gmail.com>
2016-09-18 10:35:08 +0300

    don't activate packages on init

--- init.el
+++ init.el
@@ -133,7 +133,7 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
+(package-initialize nil)
 
 (unless package-archive-contents
   (package-refresh-contents))
