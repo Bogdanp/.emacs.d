a47dfb035b9144d8cf390b1ea21176b2b98f50ae
Bogdan Popa <bogdan@defn.io>
2019-07-08 07:55:17 +0300

    racket: disable konmari for now

--- init.el
+++ init.el
@@ -784,13 +784,13 @@
   :config
   (add-hook 'racket-mode-hook #'bp-racket-mode-hook)
 
-  (flycheck-define-checker racket-konmari
-    "check racket source code using konmari"
-    :command ("raco" "konmari" "lint" source)
-    :error-patterns
-    ((error line-start (file-name) ":" line ":" column ":error:" (message) line-end)
-     (warning line-start (file-name) ":" line ":" column ":warning:" (message) line-end))
-    :modes racket-mode)
+  ;; (flycheck-define-checker racket-konmari
+  ;;   "check racket source code using konmari"
+  ;;   :command ("raco" "konmari" "lint" source)
+  ;;   :error-patterns
+  ;;   ((error line-start (file-name) ":" line ":" column ":error:" (message) line-end)
+  ;;    (warning line-start (file-name) ":" line ":" column ":warning:" (message) line-end))
+  ;;   :modes racket-mode)
 
   (add-to-list 'flycheck-checkers 'racket-konmari)
 
